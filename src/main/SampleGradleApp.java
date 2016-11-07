3cb1755f1ee21fbf1ff55386058b59d8063d0e7a
Pruthvi.Dintakurthi <Pruthvi.Dintakurthi@target.com>
2016-11-07 15:57:41 -0600

    Sample gradle app

---
+++ src/main/SampleGradleApp.java
@@ -0,0 +1,9 @@
+/**
+ * Created by a588983 on 11/7/16.
+ */
+public class SampleGradleApp {
+
+    public static void main(String[] args){
+        System.out.println("Hello, Gradle!!!!!");
+    }
+}
